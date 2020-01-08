b3d976a7425d1dbaad2c8f7d98de6ef07b365cef
Oliver Enes <oliver@enesnet.de>
2020-01-08 16:22:01 +0100

    init commit

---
+++ BatteryCheck/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BatteryCheck
+{
+    class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+        static async void Main(string[] args)
+        {
+            PowerStatus powerStatus = SystemInformation.PowerStatus;
+            float batteryLifePercent = powerStatus.BatteryLifePercent * 100;
+            while (true)
+            {
+                Console.WriteLine("Battery life: " + batteryLifePercent + "%");
+
+                if (batteryLifePercent <= 20)
+                {
+                    await client.GetAsync("192.168.1.100");
+                    //payload
+                    //var content = new StringContent("", Encoding.UTF8, "application/json");
+                    //await client.PostAsync("greetings from c#", content);
+                }
+
+                System.Threading.Thread.Sleep(5000); // thread sync
+            }
+
+        }
+    }
+}
