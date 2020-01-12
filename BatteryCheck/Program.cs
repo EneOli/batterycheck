fd4e7b584123cbfc5e94836fa43ff5252e6a2989
Oliver Enes <oliver@enesnet.de>
2020-01-12 16:20:55 +0100

    fix

--- BatteryCheck/Program.cs
+++ BatteryCheck/Program.cs
@@ -11,20 +11,27 @@ namespace BatteryCheck
     class Program
     {
         static readonly HttpClient client = new HttpClient();
-        static async void Main(string[] args)
+        static async Task Main(string[] args)
         {
             PowerStatus powerStatus = SystemInformation.PowerStatus;
             float batteryLifePercent = powerStatus.BatteryLifePercent * 100;
             while (true)
             {
+                batteryLifePercent = powerStatus.BatteryLifePercent * 100;
                 Console.WriteLine("Battery life: " + batteryLifePercent + "%");
 
                 if (batteryLifePercent <= 20)
                 {
-                    await client.GetAsync("http://192.168.1.100");
-                    //payload
-                    //var content = new StringContent("greetings from c#", Encoding.UTF8, "application/json");
-                    //await client.PostAsync("http://192.168.1.100", content);
+                    try
+                    {
+                        await client.GetAsync("http://192.168.1.100");
+                        //payload
+                        //var content = new StringContent("greetings from c#", Encoding.UTF8, "application/json");
+                        //await client.PostAsync("http://192.168.1.100", content);
+                    } catch (Exception e)
+                    {
+                        Console.WriteLine("Error: " + e.Message);
+                    }
                 }
 
                 System.Threading.Thread.Sleep(5000); // thread sync
